58e85c138a9253d3c1117770cb3a74dd806bbab2
Grzesiek <g.durtan@temida.io>
2022-09-12 22:03:07 +0200

    Update code

--- P7StateForm.ts
+++ P7StateForm.ts
@@ -1,7 +1,21 @@
-export const formSet = (form: any, e: any) => {
+
+/*
+  P7 State Form Helper
+  https://github.com/PostType7/P7-State-Form
+*/
+
+/* 
+  TODO: for contributors,
+  form array type always is: ['string: field key', {form data state}, formDataSetState()] 
+*/
+export const fieldKey = (form: any[]) => form[0]
+
+export const formSet = (form: any[] , e: React.ChangeEvent<HTMLInputElement>) => {
   form[2]({ ...form[1], ...{ [form[0]]: e.target.value } });
 };
 
-export const formGet = (form: any) => {
+export const formGet = (form: any[]) => {
   return form[1][form[0]];
 };
+
+
